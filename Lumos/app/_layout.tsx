8d4f07750ba4aa957ea6cd4a23db415110588a37
Victoria Cozma <victoria.cozma@rebeldot.com>
2025-09-07 21:53:04 +0300

    added internalization

--- Lumos/app/_layout.tsx
+++ Lumos/app/_layout.tsx
@@ -1,15 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import i18n from "../i18n";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { I18nextProvider } from "react-i18next";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
@@ -18,11 +23,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <I18nextProvider i18n={i18n}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </I18nextProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- Lumos/i18n.ts
+++ Lumos/i18n.ts
@@ -1,23 +1,31 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import * as Localization from "react-native-localize";
+import * as Localization from "expo-localization";
 
 import en from "./locales/en.json";
+// import ro from "./ro.json";
 
-// Detect device language
+const resources = {
+  en: { translation: en },
+  // ro: { translation: ro },
+};
+
+const fallback = "en";
+// get device preferred languages
 const locales = Localization.getLocales();
-const deviceLanguage = locales[0]?.languageCode || "en";
+const languageTag =
+  locales && locales.length > 0
+    ? locales[0].languageTag.split("-")[0] // "en-US" -> "en"
+    : fallback;
 
-i18n.use(initReactI18next).init({
-  //   compatibilityJSON: "v3",
-  lng: deviceLanguage, // fallback to device language
-  fallbackLng: "en",
-  resources: {
-    en: { translation: en },
-  },
+i18next.use(initReactI18next).init({
+  // compatibilityJSON: "v3",
+  resources,
+  lng: languageTag,
+  fallbackLng: fallback,
   interpolation: {
     escapeValue: false,
   },
 });
 
-export default i18n;
+export default i18next;
